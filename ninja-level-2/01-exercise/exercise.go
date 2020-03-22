5168ac6dc6acfcfa37938cf11c7f2e5f0cea3124
Paul Southall <paul.southall@192-168-1-103.tpgi.com.au>
2020-03-22 17:01:03 +1000

    Ninja level 2 - Printing types

---
+++ ninja-level-2/01-exercise/exercise.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+	//fmt.Printf("%d\t%b\t%#x", a, a, a)
+	fmt.Printf("%d\n", x)
+	fmt.Printf("%b\n", x)
+	fmt.Printf("%#x\n", x)
+}
+
+// Write a program that prints a number in decimal, binary, and hex
+// solution: https://play.golang.org/p/bAQxcEuK8O
+// video: 031
