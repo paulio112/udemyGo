8f16601168f169df4e1c0149d48e714b42caca9b
Paul Southall <paul.southall@192-168-1-103.tpgi.com.au>
2020-03-22 17:18:54 +1000

    Exercise 3: Constant use with and without type

---
+++ ninja-level-2/03-exercise/exercise.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// Can also use constants as per below.
+// const {
+// 	a = 3
+// 	b = 4
+// }
+
+func main() {
+	const a = 3
+	const b int = 4
+
+	fmt.Println(a, b)
+
+}
+
+// Create TYPED and UNTYPED constants. Print the values of the constants.
+// solution: https://play.golang.org/p/NutvJXWUx2
+// video: 033
