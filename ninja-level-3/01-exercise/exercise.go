2910e98ff46594027da147cbd9d876aaf812f642
Paul Southall <paul.southall@192-168-1-103.tpgi.com.au>
2020-03-22 20:04:19 +1000

    Ninja 3 - exercise 2: basic for loop

---
+++ ninja-level-3/01-exercise/exercise.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 10000; i++ {
+		fmt.Println(i)
+	}
+}
+
+// Print every number from 1 to 10,000
+// solution: https://play.golang.org/p/voDiuiDGGw
+// video: 050
