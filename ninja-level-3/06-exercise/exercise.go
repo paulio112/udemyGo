5b40d99ae9911154ee38a1ffd54d48d6c3f61056
Paul Southall <paul.southall@192-168-1-103.tpgi.com.au>
2020-03-23 19:21:40 +1000

    Ninja level 3 - exercise 6 : Simple if statement example

---
+++ ninja-level-3/06-exercise/exercise.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 10; i < 100; i++ {
+		if i == 11 {
+			fmt.Println("Int is 11")
+		}
+	}
+
+}
+
+// Create a program that shows the “if statement” in action.
+// solution: https://play.golang.org/p/DpZ_FLfn5s
+// video: 055
