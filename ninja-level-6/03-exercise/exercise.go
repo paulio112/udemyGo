7374cdc10cfb78f1c797277990a3a7f5a9d0fae4
Paul Southall <paul.southall@192-168-1-103.tpgi.com.au>
2020-03-26 20:11:54 +1000

    Ninja Level 6 - Exercise 3:  Simple defer use example

---
+++ ninja-level-6/03-exercise/exercise.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer foo()
+	fmt.Println("Hello, playground")
+}
+
+func foo() {
+	defer func() {
+		fmt.Println("foo DEFER ran")
+	}()
+	fmt.Println("foo ran")
+}
+
+// Use the “defer” keyword to show that a deferred func runs after the func containing it exits.
+// code: https://play.golang.org/p/XluEuUD0Nw
+// video: 104
