1676d1c4be05c7f3a3b884d798a98c72e974c2ee
Paul Southall <paul.southall@FCC7C5950E9157CA857BDD5FD1A46412.local>
2020-03-30 18:21:50 +1000

    Ninja Level 7 - Exercise 1: Outputing the address of a value

---
+++ ninja-level-7/01-exercise/exercise.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 1
+	fmt.Println(&x)
+}
+
+// Exercises - Ninja Level 7
+// Hands-on exercise #1
+// Create a value and assign it to a variable.
+// Print the address of that value.
+// code: https://play.golang.org/p/57kW8xd0qT
+// video: 116
